2791439fe39b7d4348b93f1f34e1afac389eaf64
Uanderson Ricardo <uandersonrfs@gmail.com>
2023-03-29 19:35:44 -0300

    feat: add chakra ui

---
+++ Client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Flex, Heading } from '@chakra-ui/react';
+
+const App: React.FC = () => (
+  <Flex w="100vw" h="100vh" align="center" justify="center" p="4">
+    <Heading as="h1" size="xl">Narrative Genie</Heading>
+  </Flex>
+);
+
+export default App;
