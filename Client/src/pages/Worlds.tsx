136b07812597792dabf76fd0b9c47da5ce67b939
victoria <vlsc@cin.ufpe.br>
2023-04-11 21:07:40 -0300

    feat: worlds screen

--- Client/src/pages/Worlds.tsx
+++ Client/src/pages/Worlds.tsx
@@ -18,7 +18,7 @@ import { BsFilter } from "react-icons/bs";
 
 const Worlds: React.FC = () => {
   return (
-    <>
+    <Flex direction={"column"} mb="40px">
       <Flex
         w="fit-content"
         alignSelf={"flex-end"}
@@ -26,7 +26,7 @@ const Worlds: React.FC = () => {
         py="1"
         borderRadius="3xl"
         mx="10"
-        my="-6"
+        mt="-6"
         bg="rgba(0,0,0,0.4)"
         justifyContent="flex-end"
       >
@@ -86,7 +86,7 @@ const Worlds: React.FC = () => {
         border="none"
         borderRadius="3xl"
         p="5"
-        h="250px"
+        minH="250px"
       >
         <Image
           objectFit="cover"
@@ -136,7 +136,129 @@ const Worlds: React.FC = () => {
           </CardFooter>
         </Stack>
       </Card>
-    </>
+
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+        mt="10"
+        mx="10"
+        bg="rgba(0,0,0,0.6)"
+        border="none"
+        borderRadius="3xl"
+        p="5"
+        minH="250px"
+      >
+        <Image
+          objectFit="cover"
+          borderRadius="2xl"
+          w="200px"
+          h="200px"
+          src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+          alt="Caffe Latte"
+        />
+
+        <Stack>
+          <CardBody px="4" py="0">
+            <Heading color="white" size="md">
+              Witunkles, The Spirit Vales
+            </Heading>
+
+            <Text py="1" color="white" overflow="hidden">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum...
+            </Text>
+          </CardBody>
+
+          <CardFooter justifyContent="flex-end">
+            <Button
+              variant="solid"
+              bg="#3C6C66"
+              textColor="white"
+              fontWeight="regular"
+              borderRadius="3xl"
+            >
+              Ver mais
+            </Button>
+            <Button
+              variant="solid"
+              bg="red.700"
+              textColor="white"
+              fontWeight="regular"
+              borderRadius="3xl"
+            >
+              Deletar
+            </Button>
+          </CardFooter>
+        </Stack>
+      </Card>
+
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+        mt="10"
+        mx="10"
+        bg="rgba(0,0,0,0.6)"
+        border="none"
+        borderRadius="3xl"
+        p="5"
+        minH="250px"
+      >
+        <Image
+          objectFit="cover"
+          borderRadius="2xl"
+          w="200px"
+          h="200px"
+          src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+          alt="Caffe Latte"
+        />
+
+        <Stack>
+          <CardBody px="4" py="0">
+            <Heading color="white" size="md">
+              Witunkles, The Spirit Vales
+            </Heading>
+
+            <Text py="1" color="white" overflow="hidden">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum...
+            </Text>
+          </CardBody>
+
+          <CardFooter justifyContent="flex-end">
+            <Button
+              variant="solid"
+              bg="#3C6C66"
+              textColor="white"
+              fontWeight="regular"
+              borderRadius="3xl"
+            >
+              Ver mais
+            </Button>
+            <Button
+              variant="solid"
+              bg="red.700"
+              textColor="white"
+              fontWeight="regular"
+              borderRadius="3xl"
+            >
+              Deletar
+            </Button>
+          </CardFooter>
+        </Stack>
+      </Card>
+    </Flex>
   );
 };
 
