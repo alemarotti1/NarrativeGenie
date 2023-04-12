3ededb34f38f5e199c4e20d58c181295f3a95746
victoria <vlsc@cin.ufpe.br>
2023-04-12 15:25:49 -0300

    feat: characters screen

--- Client/src/pages/Description.tsx
+++ Client/src/pages/Description.tsx
@@ -2,28 +2,32 @@ import React, { useState } from "react";
 import { Flex, Button } from "@chakra-ui/react";
 import DescriptionCard from "../components/DescriptionCard";
 import WorldHeader from "../components/WorldHeader";
+import WorldsCharacterers from "../components/WorldsCharacterers";
 import Header from "../layout/Header";
-
-
 
 const Description: React.FC = () => {
   const [current, setCurrent] = useState("Des");
   const onEdit = (newCurrent: any) => {
     setCurrent(newCurrent);
-  }
+  };
 
-const chooseTab = () =>{
-  if(current === "Des"){
-    return <DescriptionCard />
-  }else {
-    return <Button> Funfou</Button>
-  }
-};
+  const chooseTab = () => {
+    if (current === "Des") {
+      return <DescriptionCard />;
+    } else if (current === "Per") {
+      return <WorldsCharacterers />;
+    } else <></>
+  };
 
   return (
-    <Flex direction={"column"} h="full" w="full" mb="40px" alignSelf={"center"} overflowY={"hidden"}>
+    <Flex
+      direction={"column"}
+      h="full"
+      w="full"
+      alignSelf={"center"}
+    >
       <Header />
-      <WorldHeader current={current} onEdit={onEdit}/>
+      <WorldHeader current={current} onEdit={onEdit} />
       {chooseTab()}
     </Flex>
   );
