ac9b2915faff04a2ea953a0b1132c7b043e1dde0
Uanderson Ricardo <uandersonrfs@gmail.com>
2023-04-12 22:43:35 -0300

    routes and api calls of worlds

--- Client/src/components/DescriptionCard.tsx
+++ Client/src/components/DescriptionCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   Flex,
@@ -6,16 +6,39 @@ import {
   GridItem,
   Grid,
   Text,
+  Spinner,
   Textarea,
 } from "@chakra-ui/react";
 import { HiPencilAlt } from "react-icons/hi";
+import { useParams } from 'react-router-dom';
+
+import api from "../config/api";
+import environment from "../config/environment";
+
+type WorldParams = {
+  id_historia: string;
+  nome: string;
+  descricao: string;
+  path_img_capa: string;
+  email_escritor: string;
+};
 
 const DescriptionCard: React.FC = () => {
+  const { id } = useParams();
+  const [world, setWorld] = useState<WorldParams | null>(null);
+  const [loading, setLoading] = useState(true);
   const [disabled, setDisabled] = useState(true);
   const text =
     "Witunkles é um mundo mágico cheio de maravilhas incríveis e criaturas encantadas. É um lugar onde a magia é tecida no próprio tecido da existência e onde as leis da física às vezes são distorcidas ou quebradas por poderosos feitiços e encantamentos.As paisagens de Witunkles são diversas e de tirar o fôlego, variando de montanhas cobertas de neve a florestas exuberantes e praias ensolaradas. Essas paisagens costumam ser habitadas por criaturas mágicas como dragões, unicórnios, centauros e animais falantes.Em Witunkles, a magia é respeitada e temida, pois pode ser usada para o bem ou para o mal. Existem escolas de magia onde jovens feiticeiros e magos podem aprender a controlar seus poderes, e também existem bruxos das trevas que procuram usar sua magia para propósitos egoístas ou destrutivos.O povo de Witunkles é um grupo diverso, com muitas culturas e costumes diferentes. Eles estão unidos, no entanto, por seu amor compartilhado pela magia e sua reverência pelo mundo natural. Witunkles é um mundo mágico cheio de maravilhas incríveis e criaturas encantadas. É um lugar onde a magia é tecida no próprio tecido da existência e onde as leis da física às vezes são distorcidas ou quebradas por poderosos feitiços e encantamentos.As paisagens de Witunkles são diversas e de tirar o fôlego, variando de montanhas cobertas de neve a florestas exuberantes e praias ensolaradas. Essas paisagens costumam ser habitadas por criaturas mágicas como dragões, unicórnios, centauros e animais falantes.Em Witunkles, a magia é respeitada e temida, pois pode ser usada para o bem ou para o mal. Existem escolas de magia onde jovens feiticeiros e magos podem aprender a controlar seus poderes, e também existem bruxos das trevas que procuram usar sua magia para propósitos egoístas ou destrutivos.O povo de Witunkles é um grupo diverso, com muitas culturas e costumes diferentes. Eles estão unidos, no entanto, por seu amor compartilhado pela magia e sua reverência pelo mundo natural.";
   const [value, setValue] = useState(text);
   const [backup, setBackup] = useState("");
+
+  useEffect(() => {
+    api.get(`/historia/${id}`).then((res) => {
+      setWorld(res.data.story);
+      setLoading(false);
+    });
+  }, []);
 
   const handleInputChange = (e: any) => {
     const inputValue = e.target.value;
@@ -91,43 +114,31 @@ const DescriptionCard: React.FC = () => {
             )}
           </GridItem>
 
-          <GridItem area={"nav"} alignSelf="auto">
-            <Image
-              alignSelf="auto"
-              objectFit="cover"
-              borderRadius="2xl"
-              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="Caffe Latte"
-            />
-          </GridItem>
-          <GridItem
-            area={"footer"}
-            overflowY={disabled ? "scroll" : "hidden"}
-            sx={{
-              "&::-webkit-scrollbar": {
-                width: "16px",
-                borderRadius: "8px",
-                backgroundColor: "none",
-              },
-              "&::-webkit-scrollbar-thumb": {
-                borderRadius: "8px",
-                backgroundColor: `rgba(0, 0, 0, 0.8)`,
-              },
-            }}
-          >
-            {disabled ? (
-              <Text py="1" color="white" fontWeight="normal">
-                {value}
-              </Text>
-            ) : (
-              <Textarea
-                value={value}
-                bg="white"
-                maxH="full"
-                h="full"
+          {loading ? (
+            <Flex w="full" alignSelf="center" py="10">
+              <Spinner color="white" size="lg" />
+            </Flex>
+          ) : (
+            <>
+              <GridItem area={"nav"} alignSelf="auto">
+                <Image
+                  w="full"
+                  maxW="400px"
+                  h="250px"
+                  maxH="400px"
+                  alignSelf="auto"
+                  objectFit="cover"
+                  borderRadius="2xl"
+                  src={environment.API_URL + world?.path_img_capa}
+                  alt="Imagem do mundo"
+                />
+              </GridItem>
+              <GridItem
+                pl="2"
                 w="full"
-                onChange={handleInputChange}
-                overflowY="scroll"
+                h="250px"
+                area={"footer"}
+                overflowY={disabled ? "scroll" : "hidden"}
                 sx={{
                   "&::-webkit-scrollbar": {
                     width: "16px",
@@ -139,9 +150,36 @@ const DescriptionCard: React.FC = () => {
                     backgroundColor: `rgba(0, 0, 0, 0.8)`,
                   },
                 }}
-              />
-            )}
-          </GridItem>
+              >
+              {disabled ? (
+                <Text py="1" color="white" fontWeight="normal">
+                  {world?.descricao}
+                </Text>
+              ) : (
+                <Textarea
+                  value={value}
+                  bg="white"
+                  maxH="full"
+                  h="full"
+                  w="full"
+                  onChange={handleInputChange}
+                  overflowY="scroll"
+                  sx={{
+                    "&::-webkit-scrollbar": {
+                      width: "16px",
+                      borderRadius: "8px",
+                      backgroundColor: "none",
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                      borderRadius: "8px",
+                      backgroundColor: `rgba(0, 0, 0, 0.8)`,
+                    },
+                  }}
+                />
+              )}
+              </GridItem>
+            </>
+          )}
         </Grid>
       </Flex>
     </>
--- Client/src/pages/Worlds.tsx
+++ Client/src/pages/Worlds.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   Flex,
@@ -10,15 +10,34 @@ import {
   Text,
   Grid,
   GridItem,
+  Spinner,
 } from "@chakra-ui/react";
 import { HiOutlineSearch, HiOutlineFilter } from "react-icons/hi";
 import { BsFilter } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
+import api from "../config/api";
+import environment from "../config/environment";
 import Header from "../layout/Header";
-import { useNavigate } from "react-router-dom";
+
+type WorldParams = {
+  id_historia: string;
+  nome: string;
+  descricao: string;
+  path_img_capa: string;
+  email_escritor: string;
+};
 
 const Worlds: React.FC = () => {
-  const navigate = useNavigate();
+  const [worlds, setWorlds] = useState<WorldParams[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api.get("/historia", { params: { email: "teste@teste.com" }}).then((res) => {
+      setWorlds(res.data.stories || []);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <Flex direction={"column"}>
@@ -79,189 +98,96 @@ const Worlds: React.FC = () => {
         </Button>
       </Flex>
 
-      <Grid
-        mt="10"
-        mx="10"
-        bg="rgba(0,0,0,0.6)"
-        border="none"
-        borderRadius="3xl"
-        p="5"
-        gridGap={3}
-        templateAreas={`
-                  "nav main"
-                  "nav footer"`}
-        gridTemplateRows={"1fr 40px"}
-        gridTemplateColumns={"200px 1fr"}
-        h="250px"
-        color="blackAlpha.700"
-        fontWeight="bold"
-      >
-        <GridItem p="0" area={"nav"}>
-          <Image
-            w="full"
-            h="full"
-            margin="auto"
-            objectFit="cover"
-            borderRadius="2xl"
-            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-            alt="Caffe Latte"
-          />
-        </GridItem>
-        <GridItem pl="2" area={"main"} overflow="hidden">
-          <Heading color="white" size="md">
-            Witunkles, The Spirit Vales
-          </Heading>
-          <Text
-            py="1"
-            color="white"
-            fontWeight="normal"
-            maxH="200px"
-            style={{
-              display: "-webkit-box",
-              maxWidth: "full",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              WebkitLineClamp: "5",
-              WebkitBoxOrient: "vertical",
-            }}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          </Text>
-        </GridItem>
-        <GridItem
-          area={"footer"}
-          p="0"
-          display="flex"
-          justifyContent="flex-end"
-        >
-          <Button
-            variant="solid"
-            bg="#3C6C66"
-            textColor="white"
-            fontWeight="regular"
-            borderRadius="3xl"
-            mr="2"
-            onClick={() => navigate("/world")}
-          >
-            Ver mais
-          </Button>
-          <Button
-            variant="solid"
-            bg="red.700"
-            textColor="white"
-            fontWeight="regular"
-            borderRadius="3xl"
-          >
-            Deletar
-          </Button>
-        </GridItem>
-      </Grid>
-
-      <Grid
-        mt="10"
-        mx="10"
-        bg="rgba(0,0,0,0.6)"
-        border="none"
-        borderRadius="3xl"
-        p="5"
-        gridGap={3}
-        templateAreas={`
-                  "nav main"
-                  "nav footer"`}
-        gridTemplateRows={"1fr 40px"}
-        gridTemplateColumns={"200px 1fr"}
-        h="250px"
-        gap="1"
-        color="blackAlpha.700"
-        fontWeight="bold"
-      >
-        <GridItem p="0" area={"nav"}>
-          <Image
-            w="full"
-            h="full"
-            margin="auto"
-            objectFit="cover"
-            borderRadius="2xl"
-            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-            alt="Caffe Latte"
-          />
-        </GridItem>
-        <GridItem pl="2" area={"main"} overflow="hidden">
-          <Heading color="white" size="md">
-            Witunkles, The Spirit Vales
-          </Heading>
-          <Text
-            py="1"
-            color="white"
-            maxH="200px"
-            fontWeight="normal"
-            style={{
-              display: "-webkit-box",
-              maxWidth: "full",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              WebkitLineClamp: "5",
-              WebkitBoxOrient: "vertical",
-            }}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          </Text>
-        </GridItem>
-        <GridItem
-          area={"footer"}
-          p="0"
-          display="flex"
-          justifyContent="flex-end"
-        >
-          <Button
-            variant="solid"
-            bg="#3C6C66"
-            textColor="white"
-            fontWeight="regular"
-            borderRadius="3xl"
-            mr="2"
-          >
-            Ver mais
-          </Button>
-          <Button
-            variant="solid"
-            bg="red.700"
-            textColor="white"
-            fontWeight="regular"
-            borderRadius="3xl"
-          >
-            Deletar
-          </Button>
-        </GridItem>
-      </Grid>
+      {loading ? (
+        <Flex w="full" alignSelf="center" py="10">
+          <Spinner color="white" size="lg" />
+        </Flex>
+      ) : (
+        <>
+          {worlds.map(world => (
+            <Grid
+              key={world.id_historia}
+              mt="10"
+              mx="10"
+              bg="rgba(0,0,0,0.6)"
+              border="none"
+              borderRadius="3xl"
+              p="5"
+              gridGap={3}
+              templateAreas={`
+                        "nav main"
+                        "nav footer"`}
+              gridTemplateRows={"1fr 40px"}
+              gridTemplateColumns={"200px 1fr"}
+              h="250px"
+              gap="1"
+              color="blackAlpha.700"
+              fontWeight="bold"
+            >
+              <GridItem p="0" area={"nav"}>
+                <Image
+                  w="full"
+                  h="full"
+                  margin="auto"
+                  objectFit="cover"
+                  borderRadius="2xl"
+                  src={environment.API_URL + world.path_img_capa}
+                  alt="Imagem do mundo"
+                />
+              </GridItem>
+              <GridItem pl="2" area={"main"} overflow="hidden">
+                <Heading color="white" size="md">
+                  {world.nome}
+                </Heading>
+                <Text
+                  py="1"
+                  color="white"
+                  fontWeight="normal"
+                  maxH="200px"
+                  style={{
+                    display: "-webkit-box",
+                    maxWidth: "full",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    WebkitLineClamp: "5",
+                    WebkitBoxOrient: "vertical",
+                  }}
+                >
+                  {world.descricao}
+                </Text>
+              </GridItem>
+              <GridItem
+                area={"footer"}
+                p="0"
+                display="flex"
+                justifyContent="flex-end"
+              >
+                <Link to={`/worlds/${world.id_historia}`}>
+                  <Button
+                    variant="solid"
+                    bg="#3C6C66"
+                    textColor="white"
+                    fontWeight="regular"
+                    borderRadius="3xl"
+                    mr="2"
+                  >
+                    Ver mais
+                  </Button>
+                </Link>
+                <Button
+                  variant="solid"
+                  bg="red.700"
+                  textColor="white"
+                  fontWeight="regular"
+                  borderRadius="3xl"
+                >
+                  Deletar
+                </Button>
+              </GridItem>
+            </Grid>
+          ))}
+        </>
+      )}
     </Flex>
   );
 };
