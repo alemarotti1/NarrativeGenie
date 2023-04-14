6e0dfbb9d07ae2172c1bdaadaa5341a325546a82
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2023-04-13 22:44:20 -0300

    Implementação da autorização de login no servidor

--- Server/prisma/migrations/20230414000005_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `categoria` on the `personagem` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `personagem` DROP COLUMN `categoria`;
