e04304fb7af9f93ad843421431c1bd900f5964d7
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2023-04-13 22:44:20 -0300

    Implementação da autorização de login no servidor

--- Server/base_api/auth.ts
+++ Server/base_api/auth.ts
@@ -12,64 +12,57 @@ authRouter.post('/', async (req, res) => {
     //if not, send 401
     let user = null;
 
-    db.$connect().then(async () => {
-        user = await db.conta.findFirst({
-            where: {
-                email: req.body.email,
-            }
-        });
-        if(user && user.senha){
-            const match = bcrypt.compareSync(req.body.senha, user.senha);
-            if(match){
-                res.status(200).json({message: "User logged in"});
-            }
-            else{
-                res.status(401).json({message: "Wrong password"});
-            }
+    await db.$connect()
+    user = await db.conta.findFirst({
+        where: {
+            email: req.body.email,
         }
-        else{
-            res.status(401).json({message: "User not found"});
-        }
-    }).catch((err) => {
-        res.status(500).json({message: "database error"});
-    }).finally(() => {
-        db.$disconnect();
-    });       
+    });
+    let senha = "";
+    if(user && user.senha) senha = user.senha;
+
+    const match = bcrypt.compareSync(req.body.senha, senha);
+    if(match){
+        res.status(200).json({message: "User logged in"});
+    }
+    else{
+        res.status(401).json({message: "Wrong password"});
+    }
+    await db.$disconnect();
+        
 });
 
 authRouter.post('/register/', async (req, res) => {
     
 
-    db.$connect().then(async (prisma) => {
-        const hash = await bcrypt.hash(req.body.senha, saltRounds);
-        let user = await db.conta.findFirst({
-            where: {
-                AND: [
-                    {email: req.body.email},
-                ]
-            }
-        });
-        if(user){
-            res.status(409).json({message: "User already exists"});
+    await db.$connect();
+    
+    const hash = await bcrypt.hash(req.body.senha, saltRounds);
+    let user = await db.conta.findFirst({
+        where: {
+            AND: [
+                {email: req.body.email},
+            ]
         }
-        else{
-            db.conta.create({
-                data: {
-                    email: req.body.email,
-                    senha: hash,
-                }
-            }).then((user) => {
-                res.status(201).json({message: "User created"});
-            }).catch((err) => {
-                res.status(500).json({message: "database error"});
-            });
-        }
-
-    }).catch((err) => {
-        res.status(500).json({message: "database error"});
-    }).finally(() => {
-        db.$disconnect()
     });
+    if(user){
+        res.status(409).json({message: "User already exists"});
+    }
+    else{
+        db.conta.create({
+            data: {
+                email: req.body.email,
+                senha: hash,
+            }
+        }).then((user) => {
+            res.status(201).json({message: "User created"});
+        }).catch((err) => {
+            res.status(500).json({message: "database error"});
+        });
+    }
+    
+    await db.$disconnect();
+    
 
 
 });
