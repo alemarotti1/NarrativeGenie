05b78a90c378164d3ded154a0aeb75134e183128
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2023-04-13 22:00:04 -0300

    implementação do login

---
+++ Server/prisma/migrations/20230414000005_migration/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `categoria` on the `personagem` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `personagem` DROP COLUMN `categoria`;
