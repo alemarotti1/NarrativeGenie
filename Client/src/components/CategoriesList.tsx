d693acafbe5454043a57e2302f6f070526a169ef
victoria <vlsc@cin.ufpe.br>
2023-04-16 05:25:36 -0300

    fix layout

--- Client/src/components/CategoriesList.tsx
+++ Client/src/components/CategoriesList.tsx
@@ -55,7 +55,7 @@ const CategoriesList: React.FC<CategoriesListProps> = ({ items, category, onDele
             />
           </GridItem>
           <GridItem pl="2" area={"main"} overflow="hidden">
-            <Heading color="white" size="lg" fontFamily="Fondamento">
+            <Heading color="white" size="md" fontFamily="Fondamento">
               {item.nome}
             </Heading>
             <Text
@@ -82,6 +82,7 @@ const CategoriesList: React.FC<CategoriesListProps> = ({ items, category, onDele
             justifyContent="flex-end"
           >
             <Button
+            size="sm"
               variant="solid"
               bg="#3C6C66"
               marginRight="5"
@@ -95,6 +96,7 @@ const CategoriesList: React.FC<CategoriesListProps> = ({ items, category, onDele
               Ver mais
             </Button>
             <Button
+            size="sm"
               variant="solid"
               bg="red.700"
               textColor="white"
