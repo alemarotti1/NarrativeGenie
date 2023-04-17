48275d155e7d2ac9fc6fd27e460623b3028976d6
Uanderson Ricardo <uandersonrfs@gmail.com>
2023-04-16 22:20:23 -0300

    change order of models

--- Server/external/waifudiffusion.ts
+++ Server/external/waifudiffusion.ts
@@ -6,50 +6,27 @@ import { v4 as uuidv4 } from "uuid";
 import environment from "../config/environment";
 
 class WaifuDiff {
-  private readonly api;
-
-  constructor() {
-    this.api = axios.create({
-      baseURL: "https://api-inference.huggingface.co/models/runwayml/",
-      
-    });
-  }
-
   public async query(prompt: string) {
     const filename = `${uuidv4()}.jpg`;
     const filepath = path.resolve(__dirname, '..', 'images', filename);
+    let data, pathResult;
 
-    let response : AxiosResponse | null = null;
+    try {
+      data = await this.eerieOrangeMix(prompt);
+      pathResult = await this.writeImage(data, filename, filepath);
+    } catch (err) {
+      try {
+        data = await this.localDiffusion(prompt);
+        pathResult = await this.writeImageBase64(data, filename, filepath);
+      } catch (err) {
+          data = await this.waifuDiffusion(prompt);
+          pathResult = await this.writeImage(data, filename, filepath);
+      }
+    }
 
-    this.localDiffusion(prompt).then((data) => {
-      response = data;
-      //write the image to the images folder
-      this.writeImage(data, filename, filepath).then((data) => {return new Promise((r, resolve) => {resolve(data);})}).
-      catch((err) => {return new Promise((r, reject) => {reject();})});
-    })
-    
-    .catch((err) => {
-      this.eerieOrangeMix(prompt).then((data) => {
-        response = data;
-        
-        this.writeImage(data, filename, filepath).then((data) => {return new Promise((r, resolve) => {resolve(data);})}).
-        catch((err) => {return new Promise((r, reject) => {reject();})});
-      })
-        
-      .catch((err) => {
-          this.waifuDiffusion(prompt).then((data) => {
-            response = data;
-
-            this.writeImage(data, filename, filepath).then((data) => {return new Promise((r, resolve) => {resolve(data);})}).
-            catch((err) => {return new Promise((r, reject) => {reject();})});
-          })
-        });
-    });
-
-    
-
-    
+    return pathResult;
   }
+
   private async localDiffusion(prompt: string) : Promise<AxiosResponse<any>> {
     //make a get request to "https://holy-grass-05650.pktriot.net/docs" to check if the model is available
     let request = await axios.get("https://holy-grass-05650.pktriot.net/docs");
@@ -64,7 +41,7 @@ class WaifuDiff {
       "sampler_name": "DPM++ 2S a Karras",
     } 
 
-    let response = await axios.post(url, body, { responseType: 'stream' });
+    let response = await axios.post(url, body);
 
     if (response.status != 200) new Promise((r, reject) => { reject(); });
 
@@ -72,30 +49,34 @@ class WaifuDiff {
   }
 
   private async eerieOrangeMix(prompt: string) : Promise<AxiosResponse<any>> {
-    prompt = prompt + ", masterpiece, high quality, realistic,";
+    const completePrompt = prompt + ", masterpiece, high quality, realistic";
     const api = axios.create({
       baseURL: "https://api-inference.huggingface.co/models/WarriorMama777/EerieOrangeMix",
       headers: {
         Authorization: `Bearer ${environment.HUGGING_FACE_API_TOKEN}`
       }
     });
+
     let response = await api.post("/", {
-      inputs: prompt,
-      options: {
-        wait_for_model: true,
-      }
+      inputs: completePrompt,
     }, { responseType: 'stream' });
 
     if (response.status != 200) new Promise((r, reject) => { reject(); });
 
     return response;
-
   }
 
   private async waifuDiffusion(prompt: string) : Promise<AxiosResponse<any>> {
-    prompt = prompt + ", masterpiece, high quality, realistic,";
-    let response = await this.api.post("/stable-diffusion-v1-5", {
-      inputs: prompt
+    const completePrompt = prompt + ", masterpiece, high quality, realistic";
+    const api = axios.create({
+      baseURL: "https://api-inference.huggingface.co/models/runwayml/",
+      headers: {
+        Authorization: `Bearer ${environment.HUGGING_FACE_API_TOKEN}`
+      }
+    });
+
+    let response = await api.post("/stable-diffusion-v1-5", {
+      inputs: completePrompt
     }, { responseType: 'stream' });
 
     if (response.status != 200) new Promise((r, reject) => { reject(); });
@@ -107,17 +88,21 @@ class WaifuDiff {
     const writer = fs.createWriteStream(filepath);
     response.data.pipe(writer);
     
-    
     return new Promise((resolve, reject) => {
       writer.on('finish', () => resolve('images/' + filename));
       writer.on('error', () => reject());
     });
   }
 
+  private async writeImageBase64(response: AxiosResponse<any>, filename: string, filepath : string) : Promise<string> {
+    const buffer = Buffer.from(response.data.images[0], "base64");
+    fs.writeFileSync(filepath, buffer);
+    
+    return new Promise((resolve, reject) => {
+      resolve('images/' + filename);
+    });
+  }
 }
-
-
-
 
 const waifuDiff = new WaifuDiff();
 export default waifuDiff;
