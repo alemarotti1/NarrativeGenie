640347ad195318220bb1b855607bf90762c11551
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2023-04-16 22:56:02 -0300

    Prompt engineering

--- Server/helpers/prompt.ts
+++ Server/helpers/prompt.ts
@@ -1,5 +1,5 @@
 export const historiaPrompt = (prompt: string) => {
-  let result = `preencha o json com os dados de um mundo fictício descrito por "${prompt.trim()}". Adicione coisas como: as espécies/raças mais comuns, moedas, tecnologias, tipo de governo, religião, etc`;
+  let result = `preencha o json com os dados de um mundo fictício descrito por "${prompt.trim()}". Adicione coisas como: as espécies/raças mais comuns, moedas, tecnologias, tipo de governo, religião, etc na descrição. Além disso, digite ao menos dois paragrafos na descrição.`;
   result += `{ nome: string; descricao: string; prompt_para_modelo_de_imagem_em_ingles: string; }`;
   return result;
 };
@@ -11,18 +11,18 @@ export const lugarPrompt = (prompt: string) => {
 };
 
 export const outroPrompt = (prompt: string) => {
-  let result = `preencha o json com os dados de um objeto descrito por "${prompt.trim()}". onde "descricao" é uma lista de adjetivos do objeto e "descricao_fisica_em_ingles" tem a lista em inglês das características físicas mais importantes. Adicione coisas como formato, cor, material `;
+  let result = `preencha o json com os dados de um objeto descrito por "${prompt.trim()}". onde "descricao" é uma lista de adjetivos do objeto e "descricao_fisica_em_ingles" tem a lista em inglês das características físicas mais importantes. Adicione coisas como formato, cor, material em "descricao_fisica_em_ingles" e digite ao menos dois paragrafos em "descricao", descrevendo uso, história, etc. `;
   result += `{ nome: string; descricao: string; descricao_fisica_em_ingles: string[]; }`;
   return result;
 };
 
 export const personagemPrompt = (prompt: string) => {
-  let result = `preencha o json com os dados de um personagem descrito por "${prompt.trim()}". onde "descricao" é uma lista de adjetivos do personagem e "descricao_fisica_em_ingles" tem a lista em inglês das características físicas mais importantes. Adicione coisas como cor dos olhos, cor e tamanho do cabelo, cor de pele.`;
+  let result = `preencha o json com os dados de um personagem descrito por "${prompt.trim()}". onde "descricao" é uma lista de adjetivos do personagem e "descricao_fisica_em_ingles" tem a lista em inglês das características físicas mais importantes. Adicione coisas como Espécie, gênero, cor dos olhos, cor e tamanho do cabelo, cor de pele. Digite ao menos 2 paragrafos de backstory.`;
   result += `{ nome: string; descricao: string[]; backstory: string; especie: string; personalidade: string[]; descricao_fisica_em_ingles: string[]; }`;
   return result;
 };
 
 export const relacaoPrompt = (category1: string, name1: string, category2: string, name2: string, prompt: string) => {
-  let result = `escreva a relação entre um ${category1.trim()} ${name1.trim()} e um ${category2.trim()} ${name2.trim()} descrita por "${prompt.trim()}"`;
+  let result = `escreva a relação entre um ${category1.trim()} ${name1.trim()} e um ${category2.trim()} ${name2.trim()} descrita por "${prompt.trim()}". adicione coisas como: como eles se conheceram, como se relacionam, como se sentem um em relação ao outro, etc.`;
   return result;
 };
