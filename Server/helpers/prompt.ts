b955604c89d9f07b260704c93edf0f30a6232926
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2023-04-16 21:23:28 -0300

    alteração do prompt

--- Server/helpers/prompt.ts
+++ Server/helpers/prompt.ts
@@ -1,23 +1,23 @@
 export const historiaPrompt = (prompt: string) => {
-  let result = `preencha o json com os dados de um mundo fictício descrito por "${prompt.trim()}". `;
+  let result = `preencha o json com os dados de um mundo fictício descrito por "${prompt.trim()}". Adicione coisas como: as espécies/raças mais comuns, moedas, tecnologias, tipo de governo, religião, etc`;
   result += `{ nome: string; descricao: string; prompt_para_modelo_de_imagem_em_ingles: string; }`;
   return result;
 };
 
 export const lugarPrompt = (prompt: string) => {
-  let result = `preencha o json com os dados de um lugar descrito por "${prompt.trim()}". onde "descricao_fisica_em_ingles" tem a lista em inglês das características físicas e geográficas mais importantes do lugar.`;
+  let result = `preencha o json com os dados de um lugar descrito por "${prompt.trim()}". onde "descricao_fisica_em_ingles" tem a lista em inglês das características físicas e geográficas mais importantes do lugar. `;
   result += `{ nome: string; descricao: string; riqueza: int (min: 0, max: 3); saude: int (min: 0, max: 3); seguranca: int (min: 0, max: 3); agua: int (min: 0, max: 3); descricao_fisica_em_ingles: string[]; }`;
   return result;
 };
 
 export const outroPrompt = (prompt: string) => {
-  let result = `preencha o json com os dados de um objeto descrito por "${prompt.trim()}". `;
+  let result = `preencha o json com os dados de um objeto descrito por "${prompt.trim()}". onde "descricao" é uma lista de adjetivos do objeto e "descricao_fisica_em_ingles" tem a lista em inglês das características físicas mais importantes. Adicione coisas como formato, cor, material `;
   result += `{ nome: string; descricao: string; prompt_para_modelo_de_imagem_em_ingles: string; }`;
   return result;
 };
 
 export const personagemPrompt = (prompt: string) => {
-  let result = `preencha o json com os dados de um personagem descrito por "${prompt.trim()}". onde "descricao" é uma lista de adjetivos do personagem e "descricao_fisica_em_ingles" tem a lista em inglês das características físicas mais importantes.`;
+  let result = `preencha o json com os dados de um personagem descrito por "${prompt.trim()}". onde "descricao" é uma lista de adjetivos do personagem e "descricao_fisica_em_ingles" tem a lista em inglês das características físicas mais importantes. Adicione coisas como cor dos olhos, cor e tamanho do cabelo, cor de pele.`;
   result += `{ nome: string; descricao: string[]; backstory: string; especie: string; personalidade: string[]; descricao_fisica_em_ingles: string[]; }`;
   return result;
 };
